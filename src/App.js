6670c99284e26a2f552666e77e4624c33af5d583
Sanket <msanket450@gmail.com>
2022-09-25 13:46:59 +0530

    app component

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Register from "./components/Register";
+import ipConfig from "./ipConfig.json";
+import { Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import Products from "./components/Products";
+import Checkout from "./components/Checkout";
+import Thanks from "./components/Thanks";
+
+export const config = {
+  endpoint: `https://sanket-qkart-backend.herokuapp.com/api/v1`,
+};
+
+function App() {
+  return (
+    <div className="App">
+      {/* TODO: CRIO_TASK_MODULE_LOGIN - To add configure routes and their mapping */}
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/checkout" component={Checkout}></Route>
+        <Route path="/thanks" component={Thanks}></Route>
+        <Route path="/" component={Products} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
