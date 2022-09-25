a4ecb86aa6ad2bd011560b824fe79bae55507c23
Sanket <msanket450@gmail.com>
2022-09-25 13:47:13 +0530

    index page

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { SnackbarProvider } from "notistack";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/system";
+import theme from "./theme";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <SnackbarProvider
+          maxSnack={1}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          preventDuplicate
+        >
+          <App />
+        </SnackbarProvider>
+      </ThemeProvider>
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
